df9f0d97de4660e74c55bbf9d9b417f10502d6db
Tshilidzi TSHIVHULA <ttshivhu@e4r12p6.wethinkcode.co.za>
2018-06-28 09:36:40 +0200

    Fixed strcmp.s

--- strcmp.s
+++ strcmp.s
@@ -4,7 +4,7 @@ _ft_strcmp:
 	push rbp
 	mov rbp, rsp
 	xor r8, r8
-cmp:
+loop:
 	cmp [rdi + r8], byte 0
 	je results
 	cmp [rsi + r8], byte 0
@@ -13,6 +13,7 @@ cmp:
 	cmp [rdi + r8], r9
 	jne results
 	inc r8
+	jmp loop
 results:
 	mov r10, [rdi + r8]
 	mov r11, [rsi + r8]
